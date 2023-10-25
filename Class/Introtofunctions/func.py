e201e0b3bcd24302e026f4b4508ccc6aab82ff86
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2023-10-25 16:36:19 +0530

    commit2

--- Class/Introtofunctions/func.py
+++ Class/Introtofunctions/func.py
@@ -6,3 +6,17 @@ elif number == 0:
     print("Zero")
 else:
     print("Negative Number") 
+
+
+# for i  in range(5):
+#     print(i)
+
+# days = 0
+# weeks = ['Mon','Tue','Wed','Thur','Fri','Sat','Sun']
+
+# while days<7:
+#     print("Today is  " + weeks[days])
+#     days+=1
+
+# weeks[0]
+
