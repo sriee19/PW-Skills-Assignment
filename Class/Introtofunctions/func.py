d41372093a158c8202d22214f213130092958b20
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2023-10-29 15:37:37 +0530

    Flask Assignment completed

--- Class/Introtofunctions/func.py
+++ Class/Introtofunctions/func.py
@@ -1,3 +1,6 @@
+### SIMPLE LOOPS
+
+
 # number = -1
 
 # if number > 0:
@@ -11,12 +14,28 @@
 # for i  in range(5):
 #     print(i)
 
-days = 0
-weeks = ['Mon','Tue','Wed','Thur','Fri','Sat','Sun']
+# days = 0
+# weeks = ['Mon','Tue','Wed','Thur','Fri','Sat','Sun']
 
-while days<7:
-    print("Today is  " + weeks[days])
-    days+=1
+# while days<7:
+#     print("Today is  " + weeks[days])
+#     days+=1
 
-weeks[0]
+# weeks[0]
 
+
+
+###SPLIT FUNCTION
+
+test= "I write code"
+test.split()
+print(test)
+
+
+x = input("Enter 3 word message:")
+y =1
+y,w = 2,3
+
+word1,word2,word3 = x.split()
+
+word1
