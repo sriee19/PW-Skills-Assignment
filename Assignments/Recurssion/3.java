04febb247586fc07ae6df7363725b46b2133cb89
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2023-11-26 19:02:20 +0530

    3 changes

--- Assignments/Recurssion/3.java
+++ Assignments/Recurssion/3.java
@@ -2,10 +2,8 @@ public class MaxValueInArray {
     public static void main(String[] args) {
         int[] array = {13, 1, -3, 22, 5};
 
-        // Find the maximum value in the array
         int maxValue = findMaxValue(array);
 
-        // Print the result
         System.out.println("The maximum value in the array is: " + maxValue);
     }
 
@@ -14,10 +12,8 @@ public class MaxValueInArray {
             throw new IllegalArgumentException("Array is empty or null");
         }
 
-        // Initialize max value with the first element of the array
         int max = array[0];
 
-        // Iterate through the array to find the maximum value
         for (int i = 1; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
