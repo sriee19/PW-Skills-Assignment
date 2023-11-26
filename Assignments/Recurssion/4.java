4057fb2c1bc2fc7fb258d26dd49a925ca69b6e2a
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2023-11-26 19:04:34 +0530

    4 changes

--- Assignments/Recurssion/4.java
+++ Assignments/Recurssion/4.java
@@ -2,10 +2,8 @@ public class SumOfArray {
     public static void main(String[] args) {
         int[] array = {92, 23, 15, -20, 10};
 
-        // Find the sum of the values in the array
         int sum = calculateSum(array);
 
-        // Print the result
         System.out.println("The sum of the values in the array is: " + sum);
     }
 
@@ -14,10 +12,8 @@ public class SumOfArray {
             throw new IllegalArgumentException("Array is empty or null");
         }
 
-        // Initialize sum to 0
         int sum = 0;
 
-        // Iterate through the array to calculate the sum
         for (int value : array) {
             sum += value;
         }
