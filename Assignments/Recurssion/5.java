afe94cc36075b70860dd2038079b37a11eaa7f4e
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2023-11-26 19:05:48 +0530

    5 changes

--- Assignments/Recurssion/5.java
+++ Assignments/Recurssion/5.java
@@ -4,14 +4,11 @@ public class ArmstrongNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input: Read the number from the user
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
-        // Check if the number is an Armstrong number
         boolean isArmstrong = isArmstrongNumber(number);
 
-        // Output: Print the result
         if (isArmstrong) {
             System.out.println("Yes, " + number + " is an Armstrong number.");
         } else {
@@ -22,11 +19,9 @@ public class ArmstrongNumber {
     }
 
     public static boolean isArmstrongNumber(int num) {
-        // Count the number of digits in the given number
         int originalNum = num;
         int numDigits = String.valueOf(num).length();
 
-        // Calculate the sum of each digit raised to the power of the total digits
         int sum = 0;
         while (num > 0) {
             int digit = num % 10;
@@ -34,7 +29,6 @@ public class ArmstrongNumber {
             num /= 10;
         }
 
-        // Check if the sum is equal to the original number
         return sum == originalNum;
     }
 }
