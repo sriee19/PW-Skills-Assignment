8534086bc2129b9b951d24237b52a7e6e9d731cc
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2023-11-28 13:24:17 +0530

    1

---
+++ Assignments/Recurssion relation/1.py
@@ -0,0 +1,8 @@
+def recurrence_relation(n):
+    if n == 0:
+        return 5
+    else:
+        return 3 * recurrence_relation(n - 1) + 12 * n
+
+result_t2 = recurrence_relation(2)
+print("T(2) for the given recurrence relation:", result_t2)
